bbf1d3bb5d648a26cf5a63ad6fa8a074b44b4f00
William Viana <willdev.viana@gmail.com>
2022-04-19 22:05:33 -0300

    remove dependece body-parser for middlewares.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
-  res.status(200);
-  res.end();
+  res.status(200).json({ msg: 'bem vindo a API!' });
+});
+
+app.post('/auth/register', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
