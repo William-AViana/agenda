5d2d43ec5e6af6bfdbb5b29be50f97b9387d05a1
William Viana <willdev.viana@gmail.com>
2022-04-20 12:57:04 -0300

    remove route post for ./routes

--- src/app.js
+++ src/app.js
@@ -3,14 +3,12 @@ const consign = require('consign');
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'bem vindo a API!' });
-});
-
-app.post('/auth/register', (req, res) => {
-  res.status(201).json(req.body);
 });
 
 module.exports = app;
