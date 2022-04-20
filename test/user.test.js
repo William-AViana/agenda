807c0b4c6af814f507d905dd433ab012c9f3fcb4
William Viana <willdev.viana@gmail.com>
2022-04-19 22:07:02 -0300

    create test for register user

---
+++ test/user.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+
+test('Deve inserir o usuário no banco de dados', () => {
+  return request(app).post('/auth/register')
+    .send({ name: 'William', email: 'will@email.com' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('William');
+    });
+});
