027c5bdfe08c97bdc411d53f325436e957eb7809
William Viana <willdev.viana@gmail.com>
2022-04-22 22:57:29 -0300

    update test for register user

--- test/user.test.js
+++ test/user.test.js
@@ -2,11 +2,19 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-test('Deve inserir o usuário com sucesso', () => {
+test('Deve inserir o usuário com sucesso', async () => {
   return request(app).post('/auth/register')
-    .send({ name: 'William', email: 'will@email.com' })
+    .send({
+      name: 'William',
+      email: 'will@email.com',
+      passworld: '1234',
+      confirmPassworld: '1234',
+    })
     .then((res) => {
       expect(res.status).toBe(201);
-      expect(res.body.name).toBe('William');
+      expect(res.body.name).not.toBeFalsy();
+      expect(res.body.email).not.toBeFalsy();
+      expect(res.body.passworld).not.toBeFalsy();
+      expect(res.body.confirmPassworld).not.toBeFalsy();
     });
 });
