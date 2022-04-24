bd25dbfcb6cb074b3e7c743eced9e21efc995aab
William Viana <willdev.viana@gmail.com>
2022-04-24 19:28:17 -0300

    add new test for route /users and /auth/register

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,20 +1,27 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
+
+test('Deve listar todos os usuários', async () => {
+  return request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+});
 
 test('Deve inserir o usuário com sucesso', async () => {
+  const email = `${Date.now()}@email.com`;
   return request(app).post('/auth/register')
     .send({
       name: 'William',
-      email: 'will@email.com',
+      email,
       passworld: '1234',
-      confirmPassworld: '1234',
     })
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).not.toBeFalsy();
       expect(res.body.email).not.toBeFalsy();
       expect(res.body.passworld).not.toBeFalsy();
-      expect(res.body.confirmPassworld).not.toBeFalsy();
     });
 });
