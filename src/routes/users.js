c2e818f7b48cd40e06b3d143ac11fb2c76f92f3d
William Viana <willdev.viana@gmail.com>
2022-04-27 21:03:55 -0300

    refactoring for add layer service and data validate

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,14 @@
 module.exports = (app) => {
   const create = async (req, res) => {
-    const result = await app.db('users').insert(req.body, '*');
-    res.status(201).json(result[0]);
+    const result = await app.services.user.save(req.body);
+
+    if (result.error) return res.status(400).json(result);
+
+    return res.status(201).json(result[0]);
   };
 
   const findAll = async (req, res) => {
-    app.db('users').select()
+    app.services.user.findAll()
       .then((result) => res.status(200).json(result));
   };
 
