859b92dbf5379e5047c831776c8faf66b4db2e0b
William Viana <willdev.viana@gmail.com>
2022-05-19 19:35:57 -0300

    retest and changes to files generating error

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -2,8 +2,9 @@ module.exports = (app) => {
   const create = (req, res) => {
     app.services.account.save(req.body)
       .then((result) => {
-        if (result.error) return res.status(400).json(result);
         return res.status(201).json(result[0]);
+      }).cath((err) => {
+        return res.status(400).json({ error: err.message });
       });
   };
 
