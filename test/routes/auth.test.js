513d645c0b88912e46b936c6cdf1514056c39d90
William Viana <willdev.viana@gmail.com>
2022-07-07 22:43:18 -0300

    do not log in user with wrong password and email

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -16,3 +16,28 @@ test('Deve receber token ao logar', () => {
       expect(res.body).toHaveProperty('token');
     });
 });
+
+test('Não deve autenticar usuário com senha inválida.', () => {
+  const email = `${Date.now()}@email.com`;
+  return app.services.user.save({
+    name: 'William',
+    email,
+    password: '1234',
+  }).then(() => request(app).post('/auth/signin')
+    .send({
+      email, password: '4321',
+    }))
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido!');
+    });
+});
+
+test('Não deve authenticar usuário com email inválido.', () => {
+  return request(app).post('/auth/signin')
+    .send({ email: 'naoexiste@email.com', password: '4321' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido!');
+    });
+});
