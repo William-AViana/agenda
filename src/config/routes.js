613b6acdbfec901dd25fdf90168ed20cf1a9707a
William Viana <willdev.viana@gmail.com>
2022-07-09 20:32:52 -0300

    adjusting sintaxe and including authenticate with jwt

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,16 +3,17 @@ module.exports = (app) => {
     .post(app.routes.auth.signin);
 
   app.route('/users')
-    .post(app.routes.users.create);
-
-  app.route('/users')
+    .all(app.config.passport.authenticate())
+    .post(app.routes.users.create)
     .get(app.routes.users.findAll);
 
   app.route('/accounts')
+    .all(app.config.passport.authenticate())
     .get(app.routes.accounts.getAll)
     .post(app.routes.accounts.create);
 
   app.route('/accounts/:id')
+    .all(app.config.passport.authenticate())
     .get(app.routes.accounts.get)
     .put(app.routes.accounts.update)
     .delete(app.routes.accounts.remove);
